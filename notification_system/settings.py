6a45d48ab5d9ce2087fde1000d7935a228e161ab
dilna012 <dinesandilna331@gmail.com>
2026-04-30 16:06:26 +0530

    added new feature

--- notification_system/settings.py
+++ notification_system/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
