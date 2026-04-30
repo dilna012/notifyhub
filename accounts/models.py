6f7030032d50f4cedf0c4c1b3b1f54caa1aaa3c5
dilna012 <dinesandilna331@gmail.com>
2026-04-30 17:35:44 +0530

    added new feature

--- accounts/models.py
+++ accounts/models.py
@@ -43,13 +43,11 @@ class PasswordResetOTP(models.Model):
     is_used = models.BooleanField(default=False)
     attempts = models.PositiveIntegerField(default=0)
 
-    def save(self, *args, **kwargs):
-        if not self.expires_at:
-            self.expires_at = timezone.now() + timedelta(minutes=10)
-        super().save(*args, **kwargs)
-
+    
     def is_expired(self):
         return timezone.now() > self.expires_at
 
     def __str__(self):
         return f"{self.user.username} - {self.code}"
+    def get_role_display(self):
+        return dict(self.ROLE_CHOICES).get(self.role, self.role)
